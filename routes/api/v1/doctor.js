4823c7168bfff0844e689cdfda7c7691ec8a26f4
Nishant Kumar <me.nishant6217@gmail.com>
2021-07-10 00:48:55 +0530

    added final routes

--- routes/api/v1/doctor.js
+++ routes/api/v1/doctor.js
@@ -6,10 +6,6 @@ const docAPI = require("../../../controllers/api/v1/doctor");
 
 router.post("/register", docAPI.register);
 router.post("/login", docAPI.login);
-router.post(
-  "/patients/:id/create_report",
-  passport.authenticate("jwt", { session: false }),
-  docAPI.createReport
-);
+
 
 module.exports = router;
--- routes/api/v1/patient.js
+++ routes/api/v1/patient.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const passport = require("passport");
 
 const router = express.Router();
 const patAPI = require("../../../controllers/api/v1/patient");
+const docAPI = require("../../../controllers/api/v1/doctor");
 
 router.post("/register", patAPI.register);
-router.get("/patients/:id/all_reports",patAPI.allReportsOfPatient)
-router.get("/patients/:status",patAPI.allReportsOfStatus)
-
+router.get("/:id/all_reports", patAPI.allReportsOfPatient);
+router.get("/:status", patAPI.allReportsOfStatus);
+router.post(
+  "/:id/create_report",
+  passport.authenticate("jwt", { session: false }),
+  docAPI.createReport
+);
 
 module.exports = router;
